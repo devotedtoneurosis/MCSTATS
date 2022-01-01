aeec6703564bd0b312ffb0e2069c7cd2cb59e20d
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-01 14:49:12 -0330

    updates
    updates

--- MCSTATS-API/server.js
+++ MCSTATS-API/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const db = require("./app/models");
+const db = require("../models");
 console.log(models);
 db.sequelize.sync();
 
