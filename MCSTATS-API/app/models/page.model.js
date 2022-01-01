1a3952b7a78b49dda779504de91fe500ac8b8b35
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-01 16:30:40 -0330

    updates
    updates

--- MCSTATS-API/app/models/page.model.js
+++ MCSTATS-API/app/models/page.model.js
@@ -1,16 +1,19 @@
 module.exports = (sequelize, Sequelize) => {
   const Page = sequelize.define("pages", {
+  id: {
+      type: Sequelize.INTEGER
+    },
 	url: {
-      type: Sequelize.STRING(255)
+      type: Sequelize.STRING(1024)
     },
 	date: {
       type: Sequelize.DATE
     },
   title: {
-      type: Sequelize.STRING(255)
+      type: Sequelize.STRING(1024)
     },
   preview: {
-      type: Sequelize.STRING(1000)
+      type: Sequelize.STRING(5000)
     },
   weight: {
       type: Sequelize.INTEGER
