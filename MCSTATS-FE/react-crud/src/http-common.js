3300659db935541410eed3f1c4220aa1343bccf5
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-01 19:13:47 -0330

    updates
    updates

--- MCSTATS-FE/react-crud/src/http-common.js
+++ MCSTATS-FE/react-crud/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://75.127.4.251:8080/",
+  baseURL: "http://75.127.4.251:8080/api/",
   headers: {
     "Content-type": "application/json"
   }
