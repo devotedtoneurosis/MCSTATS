d022107f51e204c8b2383fe119bbd54c9b699352
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-01 16:17:12 -0330

    updates
    updates

--- MCSTATS-API/app/controllers/pages.controller.js
+++ MCSTATS-API/app/controllers/pages.controller.js
@@ -33,7 +33,7 @@ exports.create = (req, res) => {
 };
 
 // Retrieve all Pages from the database.
-exports.   = (req, res) => {
+exports.findAll  = (req, res) => {
   const title = req.query.title;
   var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
 
