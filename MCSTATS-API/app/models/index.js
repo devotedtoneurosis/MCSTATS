18ac35e45c613dd09a4c74cee79f9c52ad2a2054
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-02 12:36:15 -0330

    test
    test

--- MCSTATS-API/app/models/index.js
+++ MCSTATS-API/app/models/index.js
@@ -20,5 +20,6 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.pages = require("./page.model.js")(sequelize, Sequelize);
+db.pages = require("./stats.model.js")(sequelize, Sequelize);
 
 module.exports = db;
