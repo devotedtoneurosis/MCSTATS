db01ceb0122a7b244f9f18d1f1f5c26f9b05d8c9
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-15 15:57:07 -0330

    updates to projects service
    using /api/ url modifier

--- MCSTATS-FE/react-crud/src/services/projects.service.js
+++ MCSTATS-FE/react-crud/src/services/projects.service.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class ProjectsDataService {
   getAll() {
-    return http.get("/projects");
+    return http.get("/api/projects");
   }
 
   get(id) {
-    return http.get(`/projects/${id}`);
+    return http.get(`/api/projects/${id}`);
   }
 
   create(data) {
-    return http.post("/projects", data);
+    return http.post("/api/projects", data);
   }
 
   update(id, data) {
-    return http.put(`/projects/${id}`, data);
+    return http.put(`/api/projects/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/projects/${id}`);
+    return http.delete(`/api/projects/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/projects`);
+    return http.delete(`/api/projects`);
   }
 
   findByTitle(title) {
-    return http.get(`/projects?title=${title}`);
+    return http.get(`/api/projects?title=${title}`);
   }
 }
 
