ba5e04e1c67896dac839be237f99d603e9617541
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-15 12:48:54 -0330

    Update
    update

--- MCSTATS-FE/react-crud/src/App.js
+++ MCSTATS-FE/react-crud/src/App.js
@@ -7,8 +7,8 @@ import AddPage from "./components/add-page.component";
 import Page from "./components/page.component";
 import PagesList from "./components/pages-list.component";
 import StatsList from "./components/stat-list.component";
-import ProjectsList from "./components/page.component";
-import KeywordsList from "./components/page.component";
+import ProjectsList from "./components/project.component";
+import KeywordsList from "./components/criteria.component";
 import Page from "./components/page.component";
 
 class App extends Component {
