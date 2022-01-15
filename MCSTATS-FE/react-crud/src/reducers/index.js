09afe51e257e70d28fd0333a46780af2f321002d
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-15 16:53:58 -0330

    update
    update

--- MCSTATS-FE/react-crud/src/reducers/index.js
+++ MCSTATS-FE/react-crud/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import pages from "./pages";
+import projects from "./projects";
+import criterias from "./criterias";
 
 export default combineReducers({
   pages,
+  projects,
+  criterias,
 });
