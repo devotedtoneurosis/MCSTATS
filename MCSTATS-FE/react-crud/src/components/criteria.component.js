91770634e8a3670778c2f059cf6d635537c28a67
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-15 21:46:56 -0330

    update
    update

--- MCSTATS-FE/react-crud/src/components/criteria.component.js
+++ MCSTATS-FE/react-crud/src/components/criteria.component.js
@@ -1,200 +1,174 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { updateCriteria, deleteCriteria } from "../actions/criterias";
-import CriteriaDataService from "../services/criterias.service";
+import {
+  retrieveCriterias,
+  findCriteriasByName,
+  deleteAllCriterias,
+} from "../actions/criterias";
+import { Link } from "react-router-dom";
 
-class Criteria extends Component {
+class Criterias extends Component {
   constructor(props) {
     super(props);
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.getCriteria = this.getCriteria.bind(this);
-    this.updateStatus = this.updateStatus.bind(this);
-    this.updateContent = this.updateContent.bind(this);
-    this.removeCriteria = this.removeCriteria.bind(this);
+    this.onChangeCriteriaTitle = this.onChangeCriteriaTitle.bind(this);
+    this.refreshData = this.refreshData.bind(this);
+    this.setActiveCriteria = this.setActiveCriteria.bind(this);
+    this.findCriteriasByProjectId = this.findCriteriasByProjectId.bind(this);
+    this.removeAllCriterias = this.removeAllCriterias.bind(this);
 
     this.state = {
-      currentCriteria: {
-        id: null,
-        title: "",
-        description: "",
-        published: false,
-      },
-      message: "",
+      currentCriteria: null,
+      currentIndex: -1,
+      searchTitle: "",
     };
   }
 
   componentDidMount() {
-    this.getCriteria(this.props.match.params.id);
+    this.props.retrieveCriterias();
   }
 
-  onChangeTitle(e) {
-    const title = e.target.value;
+  onChangeCriteriaTitle(e) {
+    const searchTitle = e.target.value;
 
-    this.setState(function (prevState) {
-      return {
-        currentCriteria: {
-          ...prevState.currentCriteria,
-          title: title,
-        },
-      };
+    this.setState({
+      searchTitle: searchTitle,
     });
   }
 
-  onChangeDescription(e) {
-    const description = e.target.value;
-
-    this.setState((prevState) => ({
-      currentCriteria: {
-        ...prevState.currentCriteria,
-        description: description,
-      },
-    }));
+  refreshData() {
+    this.setState({
+      currentCriteria: null,
+      currentIndex: -1,
+    });
   }
 
-  getCriteria(id) {
-    CriteriaDataService.get(id)
+  setActiveCriteria(criteria, index) {
+    this.setState({
+      currentCriteria: criteria,
+      currentIndex: index,
+    });
+  }
+
+  removeAllCriterias() {
+    this.props
+      .deleteAllCriterias()
       .then((response) => {
-        this.setState({
-          currentCriteria: response.data,
-        });
-        console.log(response.data);
+        console.log(response);
+        this.refreshData();
       })
       .catch((e) => {
         console.log(e);
       });
   }
 
-  updateStatus(status) {
-    var data = {
-      id: this.state.currentCriteria.id,
-      title: this.state.currentCriteria.title,
-      description: this.state.currentCriteria.description,
-      published: status,
-    };
+  findCriteriasByProjectId() {
+    this.refreshData();
 
-    this.props
-      .updateTutorial(this.state.currentCriteria.id, data)
-      .then((reponse) => {
-        console.log(reponse);
-
-        this.setState((prevState) => ({
-          currentCriteria: {
-            ...prevState.currentCriteria,
-            published: status,
-          },
-        }));
-
-        this.setState({ message: "The status was updated successfully!" });
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  updateContent() {
-    this.props
-      .updateCriteria(this.state.currentCriteria.id, this.state.currentCriteria)
-      .then((reponse) => {
-        console.log(reponse);
-        
-        this.setState({ message: "The tutorial was updated successfully!" });
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  removeCriteria() {
-    this.props
-      .deleteCriteria(this.state.currentCriteria.id)
-      .then(() => {
-        this.props.history.push("/Criterias");
-      })
-      .catch((e) => {
-        console.log(e);
-      });
+    this.props.findCriteriasByProjectId(this.state.project_id);
   }
 
   render() {
-    const { currentCriteria } = this.state;
+    const { searchTitle, currentCriteria, currentIndex } = this.state;
+    const { criterias } = this.props;
 
     return (
-      <div>
-        {currentCriteria ? (
-          <div className="edit-form">
-            <h4>Criteria</h4>
-            <form>
-              <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="title"
-                  value={currentCriteria.title}
-                  onChange={this.onChangeTitle}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="description"
-                  value={currentCriteria.description}
-                  onChange={this.onChangeDescription}
-                />
-              </div>
+      <div className="list row">
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by title"
+              value={searchTitle}
+              onChange={this.onChangeSearchTitle}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.findCriteriasByProjectId}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h4>Criteria List</h4>
 
-              <div className="form-group">
+          <ul className="list-group">
+            {criterias &&
+              criterias.map((criteria, index) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setActiveCriteria(criteria, index)}
+                  key={index}
+                >
+                  {criteria.title}
+                </li>
+              ))}
+          </ul>
+
+          <button
+            className="m-3 btn btn-sm btn-danger"
+            onClick={this.removeAllCriterias}
+          >
+            Remove All
+          </button>
+        </div>
+        <div className="col-md-6">
+          {currentCriteria ? (
+            <div>
+              <h4>Keywords</h4>
+              <div>
+                <label>
+                  <strong>Title:</strong>
+                </label>{" "}
+                {currentCriteria.title}
+              </div>
+              <div>
+                <label>
+                  <strong>Description:</strong>
+                </label>{" "}
+                {currentCriteria.description}
+              </div>
+              <div>
                 <label>
                   <strong>Status:</strong>
-                </label>
+                </label>{" "}
                 {currentCriteria.published ? "Published" : "Pending"}
               </div>
-            </form>
 
-            {currentCriteria.published ? (
-              <button
-                className="badge badge-primary mr-2"
-                onClick={() => this.updateStatus(false)}
+              <Link
+                to={"/criterias/" + currentCriteria.id}
+                className="badge badge-warning"
               >
-                UnPublish
-              </button>
-            ) : (
-              <button
-                className="badge badge-primary mr-2"
-                onClick={() => this.updateStatus(true)}
-              >
-                Publish
-              </button>
-            )}
-
-            <button
-              className="badge badge-danger mr-2"
-              onClick={this.removeTutorial}
-            >
-              Delete
-            </button>
-
-            <button
-              type="submit"
-              className="badge badge-success"
-              onClick={this.updateContent}
-            >
-              Update
-            </button>
-            <p>{this.state.message}</p>
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p>Please click on a Criteria...</p>
-          </div>
-        )}
+                Edit
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Please click on a Keyword...</p>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
 }
 
-export default connect(null, { updateCriteria, deleteCriteria })(Criteria);
+const mapStateToProps = (state) => {
+  return {
+    criterias: state.criterias,
+  };
+};
+
+export default connect(mapStateToProps, {
+  retrieveCriterias,
+  findCriteriasByProjectId,
+  deleteAllCriterias,
+})(Criterias);
