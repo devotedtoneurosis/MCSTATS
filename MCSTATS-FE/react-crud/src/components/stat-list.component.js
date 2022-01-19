ed7990826cf80aeba5d83e212727ec402a847418
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-19 15:52:26 -0330

    update
    update

--- MCSTATS-FE/react-crud/src/components/stat-list.component.js
+++ MCSTATS-FE/react-crud/src/components/stat-list.component.js
@@ -18,8 +18,6 @@ class StatsList extends Component {
     this.retrievePagesByProject = this.retrievePagesByProject.bind(this);
 
     this.state = {
-      stats: null,
-      pages: null,
       currentStat: null,
       currentStatIndex: -1,
       currentPage: null,
