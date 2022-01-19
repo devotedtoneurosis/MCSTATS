b2cd80cce41f4d4e7dccbdbeb2fdf9c3753a5ca9
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-19 11:44:16 -0330

    update
    update

--- MCSTATS-FE/react-crud/src/components/stat-list.component.js
+++ MCSTATS-FE/react-crud/src/components/stat-list.component.js
@@ -18,7 +18,6 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
 
 
 
@@ -131,13 +130,13 @@ class StatsList extends Component {
       datasets: [
         {
           label: 'Dataset 1',
-          data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+          data: [],
           borderColor: 'rgb(255, 99, 132)',
           backgroundColor: 'rgba(255, 99, 132, 0.5)',
         },
         {
           label: 'Dataset 2',
-          data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+          data: [],
           borderColor: 'rgb(53, 162, 235)',
           backgroundColor: 'rgba(53, 162, 235, 0.5)',
         },
