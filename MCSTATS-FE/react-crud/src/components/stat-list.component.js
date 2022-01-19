e0772afe578323db745a4d024402319d39e9772c
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-19 12:04:28 -0330

    changing from Charts.js to Recharts
    changing from Charts.js to Recharts

--- MCSTATS-FE/react-crud/src/components/stat-list.component.js
+++ MCSTATS-FE/react-crud/src/components/stat-list.component.js
@@ -7,7 +7,7 @@ import {
   retrievePages,
 } from "../actions/pages";
 import { Link } from "react-router-dom";
-import { Line } from 'react-chartjs-2';
+import { LineChart } from 'recharts';
 
 
 
@@ -92,11 +92,14 @@ class StatsList extends Component {
 
     return (
 
-      <div>
-        <h4>Trend View</h4>
-
-        <Line />;
-      </div>
+      <LineChart width={500} height={300} data={data}>
+        <XAxis dataKey="name"/>
+        <YAxis/>
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+      </LineChart>
+    
 
     );
   }
