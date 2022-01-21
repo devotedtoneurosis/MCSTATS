3f2b56611b38c2fb8435989d850ab3e9542a6b54
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-21 13:53:40 -0330

    update
    update

--- MCSTATS-FE/react-crud/src/components/stat-list.component.js
+++ MCSTATS-FE/react-crud/src/components/stat-list.component.js
@@ -76,7 +76,7 @@ class StatsList extends Component {
 
   updateStatistics(st,pa) {
 
-    if (typeof st !== 'undefined' && typeof pa !== 'undefined' && st !== null && pa !== null  ){
+    if (typeof st !== 'undefined' && typeof pa !== 'undefined' && st !== null && pa !== null && st.length > 0 ){
       console.log("Stats and pages are defined");
       var usedPages = [];
 
