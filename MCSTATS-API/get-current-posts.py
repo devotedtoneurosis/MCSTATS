6ce96102963f1b5c1bf866b99f6cb9bcd34df76a
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-22 15:12:49 -0330

    update
    update

--- MCSTATS-API/get-current-posts.py
+++ MCSTATS-API/get-current-posts.py
@@ -152,7 +152,7 @@ def insert_record(conn,url,date,title,preview,weight,projid):
     cursor.execute("select * from pages where project_id = %s and url like %s",projid,"'"+url[:1024]+"'")
     rows = cursor.rowcount
     print("ROWS:"+str(rows))
-    if len(rows) < 1:       
+    if rows < 1:       
         now = datetime.now()
         sqlCm = "INSERT INTO pages (url,date,title,preview,weight,createdAt,updatedAt,project_id) VALUES (%s, %s, %s, %s, %s, %s, %s,%s)"
         sqlVal = (url[:1024], date, title[:1024], preview[:5000], weight,now, now,projid)
