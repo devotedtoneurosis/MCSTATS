b72b3265c7a33fa39b37eb9a79e3d34d2241c021
devotedtoneurosis <jasonpatrickwalsh@gmail.com>
2022-01-31 21:59:12 -0330

    update
    update

--- MCSTATS-FE/react-crud/src/components/stat-list.component.js
+++ MCSTATS-FE/react-crud/src/components/stat-list.component.js
@@ -18,24 +18,24 @@ import Scatter from '@bit/recharts.recharts.scatter';
 
 const CustomTooltip = ({ active, payload, label }) => {
 
-  console.log(payload);
+  console.log(payload[0].payload);
 
   if (active && payload && payload.length && 
-    typeof payload[0][2].value != 'undefined' &&
-    typeof payload[0][3].value != 'undefined') {
+    typeof payload[0].payload[2].value != 'undefined' &&
+    typeof payload[0].payload[3].value != 'undefined') {
     return (
       <div className="custom-tooltip">
-        <p className="players">Playerz:{`${label} : ${payload[0][1].value}`}</p>
-        <p className="weight">Weight:{`${label} : ${payload[0][2].value}`}</p>
-        <p className="url">URL:{`${label} : ${payload[0][3].value}`}</p>
+        <p className="players">Playerz:{`${label} : ${payload[0].payload[1].value}`}</p>
+        <p className="weight">Weight:{`${label} : ${payload[0].payload[2].value}`}</p>
+        <p className="url">URL:{`${label} : ${payload[0].payload[3].value}`}</p>
       </div>
     );
   }
 
-  if (active && payload && payload.length && typeof payload[0][1].value != 'undefined') {
+  if (active && payload && payload.length && typeof payload[0].payload[1].value != 'undefined') {
     return (
       <div className="custom-tooltip">
-        <p className="players">Players:{`${label} : ${payload[0][1].value}`}</p>
+        <p className="players">Players:{`${label} : ${payload[0].payload[1].value}`}</p>
       </div>
     );
   }
